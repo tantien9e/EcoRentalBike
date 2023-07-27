f7c89952b15f976b935b214ff64b245939781c9e
Lương Văn Tú <luongvantuit@gmail.com>
2023-07-28 00:33:31 +0700

    initial commit

---
+++ src/test/java/app/ebr/EbrApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ebr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
